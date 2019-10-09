9106f3ee9aa6c4a6a043dbdd64c04edfad9ab5de
gcacheda <CachDevs@gmail.com>
2019-10-09 19:20:15 +0200

    Component Refactor
    - New component 'Gallery' that will hold the album-items
    - Re-estructured the components folder for clarity

--- src/components/Gallery/AlbumItem.vue
+++ src/components/Gallery/AlbumItem.vue
@@ -1,25 +1,25 @@
 <template>
-    <article class="album-card">
-    	<div class="card-cover">
-      	<img :src="album.images[1].url">
-      	<div class="play-wrapper">
-          <button 
-            class="play-btn" 
-            type="button"
-            target="_blank"
-            @click="openInSpotify"
-          >
-            <font-awesome-icon icon="play" class="play-icon"/>
-          </button> 
-        </div>
+  <article class="album-card">
+    <div class="card-cover">
+      <img :src="album.images[1].url">
+      <div class="play-wrapper">
+        <button
+          class="play-btn" 
+          type="button"
+          target="_blank"
+          @click="openInSpotify"
+        >
+          <font-awesome-icon icon="play" class="play-icon"/>
+        </button> 
       </div>
-      <div class="album-card-info">
-        <h1>{{ album.name }}</h1>
-        <p>Artist: {{ album.artists[0].name }}</p>
-        <p>Realease date: {{ album.release_date }}</p>
-        <p>Total tracks: {{ album.total_tracks }}</p>
-      </div>
-      
+    </div>
+    <div class="album-card-info">
+      <h1>{{ album.name }}</h1>
+      <p>Artist: {{ album.artists[0].name }}</p>
+      <p>Realease date: {{ album.release_date }}</p>
+      <p>Total tracks: {{ album.total_tracks }}</p>
+    </div>
+    
     </article>
 </template>
 
@@ -46,65 +46,51 @@ export default {
     margin: 15px;
     border: 1px solid #ccc;
     border-radius: 8px;
-
     box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
   } 
-
 .card-cover {
   position: relative;
-
   height: 260px;
-
   background-position: -90px -20px;
   background-size: 210% auto;
   background-repeat: no-repeat;
-
   border-top-right-radius: 10px;
   border-top-left-radius: 10px;
 }
-
 .card-cover img {
   width: 100%;
   height: 260px;
 }
-
 .play-wrapper {
   position: absolute;
   top: 213px;
   left: 219px;
 }
-
 .play-btn {
   position: relative;
-
   padding: 30px;
   border: none;
   border-radius: 50%;
-
   background-color: #EAD7D7;
-
   outline: none;
 }
 
 .play-btn:hover {cursor: pointer;}
 
-
 .play-icon {
   position: absolute;
   top: 18px;
   left: 21px;
-
+  
   font-size: 25px;
   color: #fff;
 }
-
 
 .album-card-info {
   white-space: nowrap;
   
   padding: 0 15px;
 }
-
 .album-card-info h1 {
   font-size: 1.2em;
   font-weight: 600;
@@ -112,10 +98,8 @@ export default {
   color: black;
   
   margin-bottom: 10px;
-
   overflow: hidden;
   text-overflow: ellipsis;
 }
-
 .album-card-info span {color: #acb4ab;}
 </style>
---
+++ src/components/Gallery/TheGallery.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="gallery">
+      <div
+        is="album-item"
+        v-for="album in albums"
+        :key="album.id"
+        :album="album"
+      ></div>
+    </section>
+</template>
+
+<script>
+import AlbumItem from './AlbumItem.vue';
+
+export default {
+  name: 'gallery',
+  components: {
+    AlbumItem
+  },
+  props: ['albums']
+}
+</script>
+
+<style scoped>
+  .gallery {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: stretch;
+  }
+</style>
